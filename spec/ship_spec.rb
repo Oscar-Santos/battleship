e824651f60c4bf65e80ca79e07887000fbc35de0
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-07 18:06:51 -0700

    test: has a name and length pass

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Ship do
     cruiser = Ship.new("Cruiser", 3)
     expect(cruiser).to be_instance_of(Ship)
   end
+
+  it 'has a name and length' do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.name).to eq("Cruiser")
+    expect(cruiser.length).to eq(3)
+  end
 end
