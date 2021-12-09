dbc07a058d545cce94f946ae275f9e1a02204fa3
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-08 19:57:48 -0700

    test: is . if there is a ship and not fired upon pass

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -46,4 +46,12 @@ RSpec.describe Cell do
     expect(cell_1.render).to eq("M")
   end
 
+  it 'is . if there is a ship and not fired upon' do
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    cell_2.render
+    expect(cell_2.render).to eq(".")
+  end
+
 end
