43bb3d6e5239c9ce7c686492a192e0f45806597b
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-09 14:14:02 -0700

    test: exists pass

---
+++ spec/board_spec.rb
@@ -0,0 +1,10 @@
+require './lib/board'
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Board do
+  it 'exists' do
+    board = Board.new
+    expect(board).to be_instance_of(Board)
+  end
+end 
