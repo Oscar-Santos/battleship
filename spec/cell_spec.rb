1224e8bd8a28d52d3f3bc6739adbac0a6835f2a7
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-08 20:00:54 -0700

    test:is H if there is a ship and it is fired upon pass

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -62,4 +62,12 @@ RSpec.describe Cell do
     cell_2.render(true)
     expect(cell_2.render(true)).to eq("S")
   end
+
+  it 'is H if there is a ship and it is fired upon' do
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+    expect(cell_2.render).to eq("H")
+  end
 end
