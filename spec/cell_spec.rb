22dae77e5746525baccc3dbe10b45eec430c3755
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-08 19:54:27 -0700

    test: can be fired upon pass:

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -27,5 +27,14 @@ RSpec.describe Cell do
     expect(cell.empty?).to eq(false)
   end
 
+  it 'can be fired upon' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    expect(cell.fired_upon?).to eq(false)
+    cell.fire_upon
+    expect(cell.ship.health).to eq(2)
+    expect(cell.fired_upon?).to eq(true)
+  end
 
 end
