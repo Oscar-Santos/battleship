3e5ed4c55f02311dbe2e367c07ca74d6ea94aca8
oscar santos <oscar303data@gmail.com>
2021-12-12 16:15:31 -0700

    Feat: method place_cruiser is created

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,2 +1,13 @@
 class Computer
+  attr_reader :board,
+              :cruiser,
+              :submarine
+  def initialize
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
+  def place_cruiser
+  end
 end
