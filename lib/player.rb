c3c337e45da035511e7f39f8828dd052913fd88b
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-13 16:54:12 -0700

    Fix game progressing when invalid coordinates entered by refactoring place_submarine method

--- lib/player.rb
+++ lib/player.rb
@@ -13,9 +13,7 @@ class Player
     valid_coordinates = false
     until valid_coordinates == true do
       puts "Enter 3 coordinates for your cruiser. For example:   A1, B1, C1    or     A1, A2, A3"
-      cruiser = @cruiser
       cruiser_coordinates = gets.chomp.gsub(',','').split
-      # require "pry"; binding.pry
       if board.valid_placement?(cruiser, cruiser_coordinates) == true
         @board.place(cruiser, cruiser_coordinates)
         valid_coordinates = true
@@ -23,32 +21,19 @@ class Player
         puts "Invalid coordinates. Please try again."
       end
     end
-    # loop do
-    #   puts "Enter 3 coordinates for your cruiser. For example:   A1, B1, C1    or     A1, A2, A3"
-    #   cruiser = @cruiser
-    #   cruiser_coordinates = gets.chomp.gsub(',','').split
-    #   # require "pry"; binding.pry
-    #   if board.valid_placement?(cruiser, cruiser_coordinates) == true
-    #     @board.place(cruiser, cruiser_coordinates)
-    #   else
-    #     require "pry"; binding.pry
-    #     puts "Invalid coordinates. Please try again."
-    #   end
-    #   break
-    # end
   end
 
   def place_submarine
-    loop do
-      p "Enter 2 coordinates for your submarine. For example:    A1, B1   or    A1, A2"
-      submarine = @submarine
+      valid_coordinates = false
+      until valid_coordinates == true do
+      puts "Enter 2 coordinates for your submarine. For example:    A1, B1   or    A1, A2"
       submarine_coordinates = gets.chomp.gsub(',','').split
       if board.valid_placement?(submarine, submarine_coordinates) == true
         @board.place(submarine, submarine_coordinates)
+        valid_coordinates = true
       else
         p "Invalid coordinates. Please try again."
       end
-      break
     end
   end
 end
