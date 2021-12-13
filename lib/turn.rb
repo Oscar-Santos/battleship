00642e096c203618fb2778d62ec2e393cb422ad8
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-12 17:33:22 -0700

    refactor: add attributes to turn class to use in next methods being written

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,11 +4,15 @@ require './lib/player'
 
 class Turn
   attr_reader :player,
-              :computer
+              :computer,
+              :computer_sunk,
+              :player_sunk
 
   def initialize(player, computer)
     @player = player
     @computer = computer
+    @computer_sunk = 0
+    @player_sunk = 0
   end
 
   def render_boards
