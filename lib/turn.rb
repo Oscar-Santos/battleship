6e49063a9972c7efa060f8f20cef98d878ffacf7
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-12 17:01:09 -0700

    refactor: add attr_reader to turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,13 @@
+require './lib/board'
+require './lib/computer'
+require './lib/player'
+
 class Turn
+  attr_reader :player,
+              :computer
+
   def initialize(player, computer)
     @player = player
     @computer = computer
   end
+end
