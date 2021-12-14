a6194536ffc331fa75ce640e1aa214b5d590600b
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-14 13:15:55 -0700

    feat: add .upcase method to place_cruiser and place_submarine methods

--- lib/player.rb
+++ lib/player.rb
@@ -14,7 +14,7 @@ class Player
     until valid_coordinates == true do
       puts "Enter 3 coordinates for your cruiser. For example:   A1, B1, C1    or     A1, A2, A3"
       puts " "
-      cruiser_coordinates = gets.chomp.gsub(',','').split
+      cruiser_coordinates = gets.chomp.upcase.gsub(',','').split
       if board.valid_placement?(cruiser, cruiser_coordinates) == true
         @board.place(cruiser, cruiser_coordinates)
         valid_coordinates = true
@@ -29,7 +29,7 @@ class Player
       until valid_coordinates == true do
       puts "Enter 2 coordinates for your submarine. For example:    A1, B1   or    A1, A2"
       puts " "
-      submarine_coordinates = gets.chomp.gsub(',','').split
+      submarine_coordinates = gets.chomp.upcase.gsub(',','').split
       if board.valid_placement?(submarine, submarine_coordinates) == true
         @board.place(submarine, submarine_coordinates)
         valid_coordinates = true
